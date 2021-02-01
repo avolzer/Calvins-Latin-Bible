32c7c49de4088ebbdebe6307c8a4561423cdb757
April Volzer <aprilvolzer@gmail.com>
2021-01-31 19:28:59 -0500

    connected to firebase

--- App.js
+++ App.js
@@ -2,6 +2,15 @@ import React from 'react';
 import Contents from './screens/toc';
 import Navigator from './routes/drawer';
 
+// import * as firebase from 'firebase';
+// import 'firebase/firestore';
+// import firebaseConfig from './config';
+
+import {db} from './config';
+
+// Firebase: Cloud Firestore
+//export const database = firebase.firestore();
+
 export default function App() {
   global.language = 'English';
 
--- screens/reader.js
+++ screens/reader.js
@@ -1,21 +1,15 @@
 import React, {useState, useEffect, useRef} from 'react';
-import { StyleSheet, View, Text, ScrollView, Button } from "react-native";
+import { StyleSheet, View, Text, ScrollView, Button, FlatList, List } from "react-native";
 import { globalStyles } from '../styles/global';
 //import { Audio } from 'expo-av';
 import MyPlayer from '../shared/audioPlayer'
+//import textData from '../assets/csvjson'
+import {db} from '../config';
+import firebase from 'firebase';
+
 
 export default function Reader({route, navigation}) {
-    const { chapter, text } = route.params;
-
-    const playerRef = useRef();
-
-    useEffect(() => {
-        const unsubscribe = navigation.addListener('blur', () => {
-          playerRef.current.pauseVideo();
-        });
-        return unsubscribe;
-    }, [navigation]);
-
+    //const { chapter, text } = route.params;
     const fontSize=20
     const superFontSize = Math.floor(fontSize * 0.6)
     const superlineHeight = superFontSize * 1.1
@@ -58,27 +52,140 @@ export default function Reader({route, navigation}) {
         return roman;
       }
 
-   var verses = [];
-       for ( let i = 0; i < text.length; i++){
-            verses.push(
-            <View key={i} style={{flexDirection: 'row', alignItems: 'flex-start'}}>
-                {global.language=="English" ? <Text style={superScript}>{text[i].Verse}</Text> : <Text style={superScript}>{romanizeLower(text[i].Verse)}</Text>}
-                <Text style={regular}>{' ' + text[i].Text}</Text>
-            </View>
-            
-        )
-    } 
+    useEffect(() => {
+        const unsubscribe = navigation.addListener('blur', () => {
+          playerRef.current.pauseVideo();
+        });
+        return unsubscribe;
+    }, [navigation]);
+
+    const playerRef = useRef();
+
+   // const psalms = textData.filter((item)=> item.ShortBook == 'PSAL')
+
+    const [data, setData] = useState([]);
+    const [current, setCurrent] = useState(0);
+    const [loading, setLoading] = useState(false);
+    const [limit, setLimit] = useState(30);
+    const [lastVisible, setLastVisible] = useState(null)
+    const [refreshing, setRefreshing] = useState(false)
+
+    useEffect(()=> {
+      try{
+        console.log(db.ref())
+        retrieveData();
+      }
+      catch (error) {
+        console.log(error)
+      }
+      
+    }, []);
+
+    const retrieveData = async () => {
+      try{
+        setLoading(true)
+        console.log('Retrieving Data')
+
+        var ref = db.ref()
+
+        let initialQuery = db.doc('data')
+          .where('id', '<=', 40)
+          .orderBy('id')
+          .limit(limit)
+
+        let documentSnapshots = await initialQuery.get();
+
+        let documentData = documentSnapshots.docs.map(document => document.data());
+
+        console.log(documentData[documentData.length - 1])
+
+        let lastVisible = documentData[documentData.length - 1].id;
+
+        setData(documentData);
+        setLastVisible(lastVisible);
+        setLoading(false)
+      }
+      catch (error) {
+        console.log(error);
+      }
+    //   const verses = []
+    //   console.log("here it is")
+    //     for ( let i = current; i < current+200; i++){
+    //         verses.push(
+    //             psalms[i]
+    //         )
+    //         //console.log(psalms[i].Text);
+    //     } 
+    //     setData(verses)
+    //     setCurrent(current+200)
+    };
+
+    const retrieveMore = async () => {
+      try{
+        setRefreshing(true)
+        console.log('Retriving additional Data');
+
+        let additionalQuery = await db.ref('latin-app-data-default-rtdb')
+          .where('id', '<=', 40)
+          .orderBy('id')
+          .startAfter(this.state.lastVisible)
+          .limit(this.state.limit)
+
+        let documentSnapshots = await additionalQuery.get();
+
+        let documentData = documentSnapshots.docs.map(document=> document.data());
+
+        let lastVisible = documentData[documentData.length - 1].id;
+
+        setData([...data, ...documentData])
+        setLastVisible(lastVisible)
+        setRefreshing(false)
+      }
+      catch (error) {
+        console.log(error);
+      }
+    };
+
+    const renderItem = ({itemData, index}) => {
+      //console.log(data[0].Verse)
+      if(itemData !== undefined) {
+        console.log(itemData);
+        <View key={index} style={{flexDirection: 'row', alignItems: 'flex-start'}}>
+          
+            {global.language=="English" ? <Text style={superScript}>1</Text> : <Text style={superScript}>{romanizeLower(1)}</Text>}
+            <Text style={regular}>{' ' + 'hey'}</Text>
+        </View>
+      }
+      
+      }
+
+      const TheItem = ({item}) => (
+        <View>
+          <Text>{item.Text}</Text>
+        </View>
+      )
+
+    //   var verses = [];
+    //   for (let ch=0; ch<151; ch++){
+    //     const text = psalms.filter((item)=> item.Chapter==ch)
+    //     renderChapter(text);
+    //   }
+
 
     return (
         <View style={globalStyles.mainContainer}>
         <View style={globalStyles.container}>
-        <ScrollView 
-            style={styles.scroll}
-            showsVerticalScrollIndicator ={false}
-        >
-            {global.language=="English" ? <Text style={styles.chapterNum}>{chapter}</Text> : <Text style={styles.chapterNum}>{romanizeUpper(chapter)}</Text>}
-            {verses}
-        </ScrollView>
+                <FlatList 
+                style={styles.scroll}
+                data={data}
+                renderItem={({item})=><TheItem item={item} />}
+
+                //renderItem={({item, index})=>renderItem(item)}
+                onEndReachedThreshold={0.5}
+                onEndReached={retrieveMore}
+               // keyExtractor={({item}) => item.Verse.toString()}
+            />
+        
         <MyPlayer playerRef ={playerRef}/>
       </View>
       </View>
