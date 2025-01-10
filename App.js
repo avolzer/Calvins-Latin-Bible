7d35eae3e42c43eb39f2e98725032dbf4d84d3aa
avolzer <aprilvolzer@gmail.com>
2025-01-10 15:32:41 -0500

    update logo and animation

--- App.js
+++ App.js
@@ -1,32 +1,57 @@
 import "./gesture-handler";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { View } from "react-native";
 import Navigator from "./routes/tabNav";
 import { SettingsContextProvider } from "./context/settingsContext";
 import {
   createNavigationContainerRef,
   NavigationContainer,
 } from "@react-navigation/native";
+import LottieView from "lottie-react-native";
+import * as SplashScreen from "expo-splash-screen";
 
+SplashScreen.preventAutoHideAsync();
 const ref = createNavigationContainerRef();
 
 export default function App() {
   const [routeName, setRouteName] = useState();
+  const [animationCompleted, setAnimationComplete] = useState(false);
+
+  useEffect(() => {
+    SplashScreen.hideAsync();
+  }, []);
 
   return (
-    <SettingsContextProvider>
-      <NavigationContainer
-        ref={ref}
-        onReady={() => {
-          setRouteName(ref.getCurrentRoute().name);
-        }}
-        onStateChange={async () => {
-          const previousRouteName = routeName;
-          const currentRouteName = ref.getCurrentRoute().name;
-          setRouteName(currentRouteName);
-        }}
-      >
-        <Navigator routeName={routeName} />
-      </NavigationContainer>
-    </SettingsContextProvider>
+    <>
+      {!animationCompleted ? (
+        <View style={{ backgroundColor: "#1B572F" }}>
+          <LottieView
+            source={require("./assets/splash.json")}
+            style={{ width: "100%", height: "100%" }}
+            autoPlay
+            loop={false}
+            onAnimationFinish={() => {
+              setAnimationComplete(true);
+            }}
+          />
+        </View>
+      ) : (
+        <SettingsContextProvider>
+          <NavigationContainer
+            ref={ref}
+            onReady={() => {
+              setRouteName(ref.getCurrentRoute().name);
+            }}
+            onStateChange={async () => {
+              const previousRouteName = routeName;
+              const currentRouteName = ref.getCurrentRoute().name;
+              setRouteName(currentRouteName);
+            }}
+          >
+            <Navigator routeName={routeName} />
+          </NavigationContainer>
+        </SettingsContextProvider>
+      )}
+    </>
   );
 }
